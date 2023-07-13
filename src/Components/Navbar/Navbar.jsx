aee23a131e7b11f03fc792a57d0f4cda6ce49d40
Hemnath0075 <hemnath5700@gmail.com>
2023-07-13 21:54:38 +0530

    added banner

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,10 +10,11 @@ import { Input, Modal } from "antd";
 import loginImage from "../../assets/images/login.png";
 import { useForm } from "react-hook-form";
 import { UserOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
+import playStoreImage from "../../assets/images/google-play.png";
 
 function Navbar() {
-  const [userDiv,setUserDiv]=useState(false);
-  const [isLoginModal,setIsLoginModal]=useState(false);
+  const [userDiv, setUserDiv] = useState(false);
+  const [isLoginModal, setIsLoginModal] = useState(false);
   const {
     register,
     handleSubmit,
@@ -43,13 +44,13 @@ function Navbar() {
   };
   return (
     <>
-      <div className="w-full bg-white h-[10vh] border-b-2 flex items-center justify-between">
-        <div className="">
+      <div className="w-full bg-[#E3F4F4] h-[10vh] border-b-2 flex items-center justify-between">
+        <div className="flex justify-center items-center gap-[4%] ml-4">
           <img src={logo} alt="" className="h-[8vh]" />
-        </div>
-        <div className="flex justify-center items-center mr-[5%]">
-          <h3>Live Rate:-</h3>
-          <div className="w-[18vw]">
+          <div className="w-[30vw] flex flex-row items-center ">
+            <div className="">
+              <p className="whitespace-nowrap">Live Rate:-</p>
+            </div>
             <Slider {...sliderSettings}>
               <h3>
                 {" "}
@@ -65,6 +66,8 @@ function Navbar() {
               </h3>
             </Slider>
           </div>
+        </div>
+        <div className="flex justify-center items-center mr-[5%]">
           <div className="">
             <img
               src={location}
@@ -77,37 +80,58 @@ function Navbar() {
               src={user}
               alt=""
               className="w-[4vw] h-[5vh] cursor-pointer relative"
-              onMouseOver={()=>setUserDiv(true)}
-              onMouseLeave={()=>setUserDiv(false)}
+              onMouseOver={() => setUserDiv(true)}
+              onMouseLeave={() => setUserDiv(false)}
             />
-            {userDiv&&<div className="absolute w-[20vw] bg-white h-[30vh] right-[1%] top-[7%] rounded-md border-2 p-[1%] flex flex-col justify-evenly" onMouseOver={()=>setUserDiv(true)}
-              onMouseLeave={()=>setUserDiv(false)}>
-              <div className="">
-                <h3 className="font-bold">Your Account</h3>
-                <p>
-                  Track Your Orders, Manage Payment, Edit Profile And Much
-                  More..
-                </p>
+            {userDiv && (
+              <div
+                className="absolute w-[20vw] bg-white h-[30vh] right-[8%] top-[7%] rounded-md border-2 p-[1%] flex flex-col justify-evenly"
+                onMouseOver={() => setUserDiv(true)}
+                onMouseLeave={() => setUserDiv(false)}
+              >
+                <div className="">
+                  <h3 className="font-bold">Your Account</h3>
+                  <p>
+                    Track Your Orders, Manage Payment, Edit Profile And Much
+                    More..
+                  </p>
+                </div>
+                <div className="flex w-full flex-col justify-center items-center gap-4">
+                  <button
+                    className="p-[2%] bg-slate-200 border-2 flex-grow w-full rounded-md text-black font-bold"
+                    onClick={() => {
+                      showModal();
+                      setIsLoginModal(true);
+                    }}
+                  >
+                    Login
+                  </button>
+                  <button
+                    className="p-[2%] bg-slate-200 border-2 flex-grow w-full rounded-md text-black font-bold"
+                    onClick={() => {
+                      showModal();
+                      setIsLoginModal(false);
+                    }}
+                  >
+                    Signup
+                  </button>
+                </div>
               </div>
-              <div className="flex w-full flex-col justify-center items-center gap-4">
-                <button
-                  className="p-[2%] bg-slate-200 border-2 flex-grow w-full rounded-md text-black font-bold"
-                  onClick={()=>{showModal();setIsLoginModal(true)}}
-                >
-                  Login
-                </button>
-                <button className="p-[2%] bg-slate-200 border-2 flex-grow w-full rounded-md text-black font-bold" onClick={()=>{showModal();setIsLoginModal(false)}}>
-                  Signup
-                </button>
-              </div>
-            </div>}
+            )}
           </div>
           <div className="">
             <img src={bag} alt="" className="w-[4vw] h-[5vh] cursor-pointer" />
           </div>
+          <div className="">
+            <img
+              src={playStoreImage}
+              alt=""
+              className="w-auto h-[5vh] cursor-pointer"
+            />
+          </div>
         </div>
       </div>
-      <div className="w-full bg-white h-[6vh] border-b-2 flex items-center justify-evenly">
+      <div className="w-full bg-[#C4DFDF] h-[6vh] border-b-2 flex items-center justify-evenly">
         <h3 className="cursor-pointer hover:border-b-2 hover:border-blue-200">
           HOME
         </h3>
@@ -138,40 +162,130 @@ function Navbar() {
           <div className="basis-[40%] flex flex-col items-start justify-evenly w-full mt-[5%] p-[2%]">
             <h3 className="font-bold text-[4vmin]">Register With Us</h3>
             <div className="flex flex-col justify-start items-center w-full">
-              {!isLoginModal&&<form onSubmit={handleSubmit(onSubmit)} method="post" className="flex flex-col justify-center items-center w-full gap-[3vh]">
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="text" {...register("username" ,{required:true})} placeholder="Username" className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.username?'border-red-600':'border-blue-200'}`}/>
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="email" {...register("email", { required: true })} className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.email?'border-red-600':'border-blue-200'}`} placeholder="Email"/>
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="tel" {...register("mobile", { required: true })} className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.mobile?'border-red-600':'border-blue-200'}`} placeholder="Mobile Number"/>
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="password" {...register("password", { required: true })} className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.password?'border-red-600':'border-blue-200'}`} placeholder="Password"/>
-                
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input
-                  type="submit"
-                  value="Signup"
-                  className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
-                />
-                <button className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}>Already Have an Account? Login</button>
-              </form>}
-              {isLoginModal&&<form onSubmit={handleSubmit(onSubmit)} method="post" className="flex flex-col justify-center items-center w-full gap-[3vh]">
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="email" {...register("email", { required: true })} className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.email?'border-red-600':'border-blue-200'}`} placeholder="Email"/>
-                {errors.username && <span className="w-full flex justify-start text-[2vmin] text-red-500">**This field is required</span>}
-                <input type="password" {...register("password", { required: true })} className={`px-[4%] py-[3%] w-full border-2 rounded-md ${errors.password?'border-red-600':'border-blue-200'}`} placeholder="Password"/>
-                
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input
-                  type="submit"
-                  value="Login"
-                  className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
-                />
-                <button className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}>Don't Have an Account? Register Here</button>
-              </form>}
+              {!isLoginModal && (
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  method="post"
+                  className="flex flex-col justify-center items-center w-full gap-[3vh]"
+                >
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="text"
+                    {...register("username", { required: true })}
+                    placeholder="Username"
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.username ? "border-red-600" : "border-blue-200"
+                    }`}
+                  />
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.email ? "border-red-600" : "border-blue-200"
+                    }`}
+                    placeholder="Email"
+                  />
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="tel"
+                    {...register("mobile", { required: true })}
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.mobile ? "border-red-600" : "border-blue-200"
+                    }`}
+                    placeholder="Mobile Number"
+                  />
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="password"
+                    {...register("password", { required: true })}
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.password ? "border-red-600" : "border-blue-200"
+                    }`}
+                    placeholder="Password"
+                  />
+
+                  {/* errors will return when field validation fails  */}
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                  <input
+                    type="submit"
+                    value="Signup"
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
+                  />
+                  <button
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
+                  >
+                    Already Have an Account? Login
+                  </button>
+                </form>
+              )}
+              {isLoginModal && (
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  method="post"
+                  className="flex flex-col justify-center items-center w-full gap-[3vh]"
+                >
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.email ? "border-red-600" : "border-blue-200"
+                    }`}
+                    placeholder="Email"
+                  />
+                  {errors.username && (
+                    <span className="w-full flex justify-start text-[2vmin] text-red-500">
+                      **This field is required
+                    </span>
+                  )}
+                  <input
+                    type="password"
+                    {...register("password", { required: true })}
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md ${
+                      errors.password ? "border-red-600" : "border-blue-200"
+                    }`}
+                    placeholder="Password"
+                  />
+
+                  {/* errors will return when field validation fails  */}
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                  <input
+                    type="submit"
+                    value="Login"
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
+                  />
+                  <button
+                    className={`px-[4%] py-[3%] w-full border-2 rounded-md cursor-pointer bg-[#DE0606] text-white font-bold`}
+                  >
+                    Don't Have an Account? Register Here
+                  </button>
+                </form>
+              )}
             </div>
           </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Slider from "react-slick";
+import Banner1 from '../../assets/images/banner1.png'
+import Banner2 from '../../assets/images/banner2.png'
+import Banner3 from '../../assets/images/banner3.png'
+import axios from "axios";
 
 function Home() {
+  useEffect(()=>{
+    const getData=async()=>{
+      const res=await axios.get('https://savingsapi.vedhatechnology.com/api/GetTodaysRate',{
+      "Date":"2022-09-16"
+    })
+    console.log(res)
+    }
+    getData();
+  },[])
+  const sliderSettings = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    fade:true
+  };
   return (
-    <div className="min-h-screen w-full">
+    <div className="min-h-screen w-full bg-[#F8F6F4]">
       <Navbar/>
+      <div className="">
+        <div className="">
+        <Slider {...sliderSettings}>
+                    <img src={Banner1} alt="" />
+                    <img src={Banner2} alt="" />
+                    <img src={Banner3} alt="" />
+        </Slider>
+        </div>
+      </div>
     </div>
   );
 }
