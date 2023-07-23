07625ef888e5c99cfe35bbf84d9c22ee80a2aaf3
Hemnath0075 <hemnath5700@gmail.com>
2023-07-23 21:19:17 +0530

    done cards and footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,98 @@
-import React from 'react'
+import React from "react";
+import location from "../../assets/icons/home_pin_FILL0_wght400_GRAD0_opsz48.svg";
+import landline from "../../assets/icons/call_FILL0_wght400_GRAD0_opsz48.svg";
+import mobile from "../../assets/icons/phone_iphone_FILL0_wght400_GRAD0_opsz48.svg";
+import mail from "../../assets/icons/mail_FILL0_wght400_GRAD0_opsz48.svg";
+import instagram from "../../assets/icons/instagram-1-svgrepo-com.svg";
+import facebook from "../../assets/icons/facebook-color-svgrepo-com.svg";
+import twitter from "../../assets/icons/twitter-color-svgrepo-com.svg";
+import whatsapp from "../../assets/icons/whatsapp-svgrepo-com.svg";
 
 function Footer() {
   return (
-    <div className="w-full h-[30vh] bg-[#DADADA] bottom-0">
-      <div className="">
-
+    <div className="w-full h-[40vh] bg-[#ffffff] bottom-0 flex flex-col footer">
+      <div className="flex flex-row pr-[2vw] pl-[2vw] pt-[2vw] h-full justify-between w-[100vw]">
+        <div className="h-full">
+          <h2 className="text-[3vmin] font-bold border-b-2 border-black">
+            Products
+          </h2>
+          <div className="flex flex-col justify-around">
+            <a href="#">Purchase Plans</a>
+            <a href="#">Gold</a>
+            <a href="#">Silver</a>
+            <a href="#">Diamonds</a>
+            <a href="#">Platinum</a>
+          </div>
+        </div>
+        <div className="h-full">
+          <h2 className="text-[3vmin] font-bold  border-b-2 border-black">
+            Useful Links
+          </h2>
+          <div className="flex flex-col justify-around">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Direct Me</a>
+            <a href="#">Terms and Conditions</a>
+            <a href="#">Refund Policy</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Shipping Policy</a>
+          </div>
+        </div>
+        <div className="flex flex-col h-full font-bold">
+          <h2 className="text-[3vmin]  border-b-2 border-black font-bold">
+            Contact
+          </h2>
+          <div className="flex flex-col justify-between flex-auto">
+            <div className="flex flex-row justify-center items-center">
+              <img src={location} alt="" className="w-[3vw] h-[5vh]" />
+              <p className="w-[20vw]">
+                {
+                  "Address:-East Car Street, TVAN Corner,Tiruchengode - 637 211. Namakkal District."
+                }
+              </p>
+            </div>
+            <div className="flex flex-row justify-center items-center">
+              <img src={landline} alt="" className="w-[2vw] h-[5vh]" />
+              <p className="w-[20vw]">{"(04288) 252591, 256081, 253591"}</p>
+            </div>
+            <div className="flex flex-row justify-center items-center">
+              <img src={mobile} alt="" className="w-[2vw] h-[5vh]" />
+              <p className="w-[20vw]">{"+91 97885 35555"}</p>
+            </div>
+            <div className="flex flex-row justify-center items-center">
+              <img src={mail} alt="" className="w-[2vw] h-[4vh]" />
+              <p className="w-[20vw]">{"tvangold@gmail.com"}</p>
+            </div>
+          </div>
+        </div>
+        <div className="h-full flex flex-col items-center gap-1">
+          <iframe
+            className="mb-1"
+            title="id"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.379126083216!2d77.89736119999999!3d11.3799859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9619122a5c2ff%3A0x72af12867473ee36!2sTVAN%20Jewellers!5e0!3m2!1sen!2sin!4v1690105998192!5m2!1sen!2sin"
+            width="300"
+            height="200"
+            style={{ border: "0" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
-      <div className="">
-        
+      <div className="flex flex-row w-full justify-between items-center bg-[#FCF9F9] p-4">
+        <div className=""></div>
+        <div className="">
+          <p>© 2023 GRT Jewellers Private Limited. All rights reserved.</p>
+        </div>
+        <div className="flex flex-row gap-4 justify-between">
+          <img src={instagram} alt="" className="w-[2vw] h-[4vh]" />
+          <img src={facebook} alt="" className="w-[2vw] h-[4vh]" />
+          <img src={twitter} alt="" className="w-[2vw] h-[4vh]" />
+          <img src={whatsapp} alt="" className="w-[2vw] h-[4vh]" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,33 @@
 import React, { useEffect } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Slider from "react-slick";
-import Banner1 from '../../assets/images/banner1.png'
-import Banner2 from '../../assets/images/banner2.png'
-import Banner3 from '../../assets/images/banner3.png'
+import Banner1 from "../../assets/images/banner1.png";
+import Banner2 from "../../assets/images/banner2.png";
+import Banner3 from "../../assets/images/banner3.png";
 import axios from "axios";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import PrevArrow from '../../assets/icons/arrow_back_ios_FILL0_wght400_GRAD0_opsz48.svg'
-import NextArrow from '../../assets/icons/arrow_forward_ios_FILL0_wght400_GRAD0_opsz48.svg'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import PrevArrow from "../../assets/icons/arrow_back_ios_FILL0_wght400_GRAD0_opsz48.svg";
+import NextArrow from "../../assets/icons/arrow_forward_ios_FILL0_wght400_GRAD0_opsz48.svg";
 import Footer from "../../Components/Footer/Footer";
-
+import goldScheme1 from "../../assets/images/goldscheme1.png";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", right:'10px',zIndex:'1',color:'white'}}
+      style={{
+        ...style,
+        display: "block",
+        right: "10px",
+        zIndex: "1",
+        color: "white",
+      }}
       onClick={onClick}
     >
       <img src={NextArrow} alt="" />
-      </div>
+    </div>
   );
 }
 
@@ -30,29 +36,58 @@ function SamplePrevArrow(props) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block",left:'10px',color:'white',zIndex:'1' }}
+      style={{
+        ...style,
+        display: "block",
+        left: "10px",
+        color: "white",
+        zIndex: "1",
+      }}
       onClick={onClick}
     >
       <img src={PrevArrow} alt="" />
-      </div>
+    </div>
   );
 }
 
-
 function Home() {
   // useEffect(()=>{
-  //   // fetch('https://savingsapi.vedhatechnology.com/api/GetTodaysRate').then(()=>console.log("done"))
-  //   const getData=async()=>{
-  //     const res=await axios.post('https://savingsapi.vedhatechnology.com/api/GetTodaysRate',{
+  // fetch('https://savingsapi.vedhatechnology.com/api/GetTodaysRate').then(()=>console.log("done"))
+  // const getData=async()=>{
+  //   const res=await axios.post('https://savingsapi.vedhatechnology.com/api/GetTodaysRate',{
+  //   "Date":"2022-09-16",
+  //   headers: {
+  //     "Cache-Control": "no-cache",
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //   },
+  // },)
+  // console.log(res)
+  // }
+  // let loginInfo = {
+
+  // };
+  // fetch('https://savingsapi.vedhatechnology.com/web/api/login', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-type': 'application/json'
+  //   },
+  //   body: {
+  //     "Username":"hemnath",
+  //     "Password":"hemnath",
+  //   },
+  // })
+  // fetch('https://savingsapi.vedhatechnology.com/api/GetTodaysRate', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-type': 'application/json'
+  //   },
+  //   body: {
   //     "Date":"2022-09-16",
-  //     headers: {
-  //       "Cache-Control": "no-cache",
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //     },
-  //   },)
-  //   console.log(res)
-  //   }
-  //   getData();
+  //   },
+  // })
+  // getData();
   // },[])
   const sliderSettings = {
     slidesToShow: 1,
@@ -61,23 +96,91 @@ function Home() {
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: true,
-    fade:true,
-    nextArrow:<SampleNextArrow/>,
-    prevArrow:<SamplePrevArrow/>,
+    fade: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <div className="min-h-screen w-full bg-[#F8F6F4]">
-      <Navbar/>
+      <Navbar />
       <div className="">
         <div className="">
-        <Slider {...sliderSettings}>
-                    <img src={Banner1} alt="" />
-                    <img src={Banner2} alt="" />
-                    <img src={Banner3} alt="" />
-        </Slider>
+          <Slider {...sliderSettings}>
+            <img src={Banner1} alt="" />
+            <img src={Banner2} alt="" />
+            <img src={Banner3} alt="" />
+          </Slider>
+        </div>
+        <div className="w-full flex justify-center items-center flex-col">
+          <h2 className="text-[4vmin]">Gold Savings Scheme</h2>
+          <div className="flex flex-row px-[1%] w-full justify-between">
+            <div className="w-[30vw] h-auto bg-white rounded-md card">
+              <img
+                src="https://www.radhajewellers.com/images/feature-pic-2.jpg"
+                alt=""
+                className="w-[40vw] h-[30vh] rounded-t-md"
+              />
+              <div className="p-4">
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[15px] h-[15px] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-[30vw] h-auto bg-white rounded-md card">
+              <img
+                src="https://www.radhajewellers.com/images/feature-pic-2.jpg"
+                alt=""
+                className="w-[40vw] h-[30vh] rounded-t-md"
+              />
+              <div className="p-4">
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-[30vw] h-auto bg-white rounded-md card">
+              <img
+                src="https://www.radhajewellers.com/images/feature-pic-2.jpg"
+                alt=""
+                className="w-[40vw] h-[30vh] rounded-t-md"
+              />
+              <div className="p-4">
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+                <div className="flex flex-row justify-start items-center gap-4">
+                  <div className="w-[1vw] h-[2vh] bg-black rounded-[50%]"></div>
+                  <p>Monthly Amount:- ₹ 100,₹ 1000,₹ 10000,₹ 100000,</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
