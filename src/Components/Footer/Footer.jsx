1a7bae881eba94ca0afdbfa1e72b8dfcfa62e8df
Hemnath0075 <hemnath5700@gmail.com>
2023-07-30 22:59:30 +0530

    done footbar

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
   return (
     <div className="w-full h-[40vh] bg-[#ffffff] bottom-0 flex flex-col footer">
       <div className="flex flex-row p-[2vw] h-full justify-between w-[100vw]">
-        <div className="h-full">
+        {/* <div className="h-full">
           <h2 className="text-[3vmin] font-bold border-b-2 border-black">
             Products
           </h2>
@@ -23,7 +23,7 @@ function Footer() {
             <a href="#">Diamonds</a>
             <a href="#">Platinum</a>
           </div>
-        </div>
+        </div> */}
         <div className="h-full">
           <h2 className="text-[3vmin] font-bold  border-b-2 border-black">
             Useful Links
@@ -39,7 +39,7 @@ function Footer() {
           </div>
         </div>
         <div className="flex flex-col h-full font-bold">
-          <h2 className="text-[3vmin]  border-b-2 border-black font-bold">
+          <h2 className="text-[3vmin]  border-b-2 w-auto border-black font-bold">
             Contact
           </h2>
           <div className="flex flex-col justify-between flex-auto">
@@ -65,6 +65,21 @@ function Footer() {
             </div>
           </div>
         </div>
+        <div className="h-full mr-[5%]">
+          <h2 className="text-[3vmin] font-bold  border-b-2 border-black">
+            Write to Us
+          </h2>
+          <div className="flex flex-col justify-around">
+              <input type="text" className="bg-white border-b-2 w-[150%] p-2" placeholder="Full Name"/>
+              <input type="text" className="bg-white border-b-2 w-[150%] p-2" placeholder="Mobile Number"/>
+              <input type="text" className="bg-white border-b-2 w-[150%] p-2" placeholder="Email"/>
+              <input type="text" className="bg-white border-b-2 w-[150%] p-2" placeholder="Feedback Message"/>
+              <div className="flex w-[150%] justify-end">
+              <input type="submit" value="Submit" className="bg-white border-2 border-red-500 w-[50%] px-2 py-1 mt-1" placeholder="Feedback Message"/>
+              </div>
+              
+          </div>
+        </div>
         <div className="h-full flex flex-col items-center gap-1">
           <iframe
             className="mb-1"
@@ -77,12 +92,12 @@ function Footer() {
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
-          <div className="flex flex-row gap-4 justify-between w-[80%]">
+          {/* <div className="flex flex-row gap-4 justify-between w-[80%]">
           <img src={instagram} alt="" className="w-[2vw] h-[4vh]" />
           <img src={facebook} alt="" className="w-[2vw] h-[4vh]" />
           <img src={twitter} alt="" className="w-[2vw] h-[4vh]" />
           <img src={whatsapp} alt="" className="w-[2vw] h-[4vh]" />
-        </div>
+        </div> */}
         </div>
       </div>
       <div className="flex flex-row w-full justify-center items-center bg-[#cbeef2] text-black p-4">
